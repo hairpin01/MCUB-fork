8ccfe5e7bd9aa71b549bc76bc645acdf3add1d78
Шмэлька <alichka240784@gmail.com>
2026-01-28 23:16:07 +1000

    update __init___ for utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,86 +1,72 @@
-
 # author: @Hairpin00
-# version: 1.0.0
+# version: 1.0.1
 # description: utils package initialization
 
-try:
-    from .platform import (
-        PlatformDetector,
-        get_platform,
-        get_platform_info,
-        get_platform_name,
-        is_termux,
-        is_wsl,
-        is_vds,
-        is_docker,
-        is_mobile,
-        is_desktop,
-        is_virtualized
-    )
-    PLATFORM_AVAILABLE = True
-except ImportError:
-    PLATFORM_AVAILABLE = False
+from .platform import (
+    PlatformDetector,
+    get_platform,
+    get_platform_info,
+    get_platform_name,
+    is_termux,
+    is_wsl,
+    is_vds,
+    is_docker,
+    is_mobile,
+    is_desktop,
+    is_virtualized
+)
+
+from . import platform
+
+__all__ = [
+    'PlatformDetector',
+    'get_platform',
+    'get_platform_info',
+    'get_platform_name',
+    'is_termux',
+    'is_wsl',
+    'is_vds',
+    'is_docker',
+    'is_mobile',
+    'is_desktop',
+    'is_virtualized',
+    'platform'
+]
+
 
 try:
     from .html_parser import parse_html, telegram_to_html
-    HTML_PARSER_AVAILABLE = True
+    __all__.extend(['parse_html', 'telegram_to_html'])
 except ImportError:
-    HTML_PARSER_AVAILABLE = False
+    pass
 
 try:
     from .emoji_parser import emoji_parser
-    EMOJI_PARSER_AVAILABLE = True
+    __all__.extend(['emoji_parser'])
 except ImportError:
-    EMOJI_PARSER_AVAILABLE = False
+    pass
 
 try:
     from .message_helpers import edit_with_html, reply_with_html, send_with_html, send_file_with_html
-    MESSAGE_HELPERS_AVAILABLE = True
+    __all__.extend(['edit_with_html', 'reply_with_html', 'send_with_html', 'send_file_with_html'])
 except ImportError:
-    MESSAGE_HELPERS_AVAILABLE = False
+    pass
 
-# Export platform utilities
-if PLATFORM_AVAILABLE:
-    __all__ = [
-        'PlatformDetector',
-        'get_platform',
-        'get_platform_info',
-        'get_platform_name',
-        'is_termux',
-        'is_wsl',
-        'is_vds',
-        'is_docker',
-        'is_mobile',
-        'is_desktop',
-        'is_virtualized',
-        'platform'
-    ]
-else:
-    __all__ = []
+__version__ = '1.0.1'
+__author__ = '@Hairpin00'
+__description__ = 'MCUB Utils Package'
 
-# Export other utilities if available
-if HTML_PARSER_AVAILABLE:
-    __all__.extend(['parse_html', 'telegram_to_html', 'html_parser'])
-
-if EMOJI_PARSER_AVAILABLE:
-    __all__.extend(['emoji_parser'])
-
-if MESSAGE_HELPERS_AVAILABLE:
-    __all__.extend(['edit_with_html', 'reply_with_html', 'send_with_html', 'send_file_with_html', 'message_helpers'])
-
-
-# Optional: create a main utility function for checking all utils availability
 def get_utils_status():
     """Returns the availability status of all utilities"""
-    return {
-        'platform': PLATFORM_AVAILABLE,
-        'html_parser': HTML_PARSER_AVAILABLE,
-        'emoji_parser': EMOJI_PARSER_AVAILABLE,
-        'message_helpers': MESSAGE_HELPERS_AVAILABLE,
+    status = {
+        'platform': 'platform' in globals(),
+        'html_parser': 'parse_html' in globals(),
+        'emoji_parser': 'emoji_parser' in globals(),
+        'message_helpers': 'edit_with_html' in globals(),
         'version': __version__
     }
+    return status
 
-# Add the status function to exports if needed
 __all__.append('get_utils_status')
 
 # # Для обратной совместимости

c6ba060c8b7526cd71a6374db81607ef55a91f5a
Шмэлька <alichka240784@gmail.com>
2026-02-02 01:01:34 +1000

    update __init__

--- core_inline/__init__.py
+++ core_inline/__init__.py
@@ -1,5 +1,6 @@
 from .bot import InlineBot
 from .handlers import InlineHandlers
 from .keyboards import InlineKeyboards
+from .command import setup_bot_commands
 
-__all__ = ['InlineBot', 'InlineHandlers', 'InlineKeyboards']
+__all__ = ['InlineBot', 'InlineHandlers', 'InlineKeyboards', 'setup_bot_commands']

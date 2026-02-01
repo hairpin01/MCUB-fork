81e8aa6ae046fc2c47c79e1d19b9fcd6b76ee825
Шмэлька <alichka240784@gmail.com>
2026-02-02 01:34:20 +1000

    update MCUB.py

--- MCUB.py
+++ MCUB.py
@@ -21,7 +21,6 @@ if __name__ == '__main__':
         asyncio.run(main())
     except KeyboardInterrupt:
         print('\nStop Kernel...')
-        Kernel.client.disconnect()
         sys.exit(0)
         
 

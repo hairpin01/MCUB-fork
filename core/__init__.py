b60659ed1ecd33eb0805b3032e30cec9eb627e44
Шмэлька <alichka240784@gmail.com>
2026-02-02 19:08:21 +1000

    update cpre/__init__

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,7 @@
 # author: @Hairpin00
-# version: 1.0.0
+# version: 1.0.1
 # description: init
-from .kernel import Kernel
-from .kernel import Colors
 
-__all__ = ['Kernel', 'Colors']
+from .kernel import Kernel, Colors, Register, CallbackPermissionManager
+
+__all__ = ['Kernel', 'Colors', 'Register', 'CallbackPermissionManager']

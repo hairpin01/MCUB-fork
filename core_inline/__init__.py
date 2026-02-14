2952374a00ed1eba57bb86451f62282621f0dac1
Шмэлька <alichka240784@gmail.com>
2026-02-14 19:26:08 +1000

    delete command.py and update __init__

--- core_inline/__init__.py
+++ core_inline/__init__.py
@@ -1,6 +1,6 @@
 from .bot import InlineBot
 from .handlers import InlineHandlers
 from .keyboards import InlineKeyboards
-from .command import setup_bot_commands
+# from .command import setup_bot_commands
 
 __all__ = ["InlineBot", "InlineHandlers", "InlineKeyboards", "setup_bot_commands"]
--- core_inline/command.py
+++
@@ -1,6 +0,0 @@
-# author: @Hairpin00
-# version: 1.0.1
-from telethon import events, Button
-
-async def setup_bot_commands(bot_client, kernel):
-    pass

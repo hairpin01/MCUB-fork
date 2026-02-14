1272c41dcc110acfe2ee0e2097c4f0660f8d6bd5
Шмэлька <alichka240784@gmail.com>
2026-02-14 18:02:32 +1000

    update __init__

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,23 @@
 # author: @Hairpin00
 # version: 1.0.1
-# description: init
+# description: Core package initialization
 
-from .kernel import Kernel, Colors, Register, CallbackPermissionManager
+# Import all classes from their new locations
+from .colors import Colors
+from .exceptions import CommandConflictError
+from .cache import TTLCache
+from .scheduler import TaskScheduler
+from .register import Register
+from .permissions import CallbackPermissionManager
+from .kernel import Kernel
 
-__all__ = ["Kernel", "Colors", "Register", "CallbackPermissionManager"]
+
+__all__ = [
+    "Kernel",
+    "Colors",
+    "Register",
+    "CallbackPermissionManager",
+    "TTLCache",
+    "TaskScheduler",
+    "CommandConflictError",
+]

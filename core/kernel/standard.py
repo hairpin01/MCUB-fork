7ef7bdfa9a492e4c62ed8c6ec5b0d051a961c833
Шмэлька <alichka240784@gmail.com>
2026-03-01 18:56:43 +1000

    update standard kernel

--- core/kernel/standard.py
+++ core/kernel/standard.py
@@ -278,7 +278,7 @@ class Kernel:
         import time
 
         _REQUIREMENTS = [
-            ("telethon", "telethon"),
+            ("telethon", "telethon_mcub"),
             ("aiohttp", "aiohttp"),
             ("aiohttp-jinja2", "aiohttp_jinja2"),
             ("jinja2", "jinja2"),
